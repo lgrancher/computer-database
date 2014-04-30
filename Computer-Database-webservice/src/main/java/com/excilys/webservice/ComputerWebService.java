20e8ae6c6241cdcb8bda1d44177b545bcf3b9995
lgrancher <lgrancher@excilys.com>
2014-04-30 11:35:32 +0200

    Jax-RS

--- Computer-Database-webservice/src/main/java/com/excilys/webservice/ComputerWebService.java
+++ Computer-Database-webservice/src/main/java/com/excilys/webservice/ComputerWebService.java
@@ -3,20 +3,26 @@ package com.excilys.webservice;
 import java.util.List;
 
 import javax.jws.WebService;
+import javax.ws.rs.GET;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.excilys.om.Computer;
 import com.excilys.service.ComputerService;
-
+ 
 @WebService
+@Component
 public class ComputerWebService 
-{
+{ 
 	@Autowired
 	private ComputerService computerService;
 	
-	public List<Computer> retrieve()
+	@GET
+	public Response getComputers() 
 	{
-		return computerService.findAll();
+		List<Computer> result = computerService.findAll();
+		return Response.status(200).entity(result.toString()).build();
 	}
 }
